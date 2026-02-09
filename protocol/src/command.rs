cd4e782b16941ccc4983c90203e23fa4cb70ce3c
Anton Chirkunov <41851+achirkunov@users.noreply.github.com>
2026-02-09 21:44:56 +0000

    Implement Command::from_frame with Result return type and PING support

--- protocol/src/command.rs
+++ protocol/src/command.rs
@@ -1,15 +1,27 @@
-#[derive(Clone)] // TODO: to remove
+use crate::resp::RespFrame;
+
+#[derive(Clone, Debug, PartialEq)] // TODO: to remove
 pub enum Value {
     String(String),
     Integer(i64),
     List(Vec<Value>),
 }
 
+#[derive(Debug, PartialEq)]
 pub enum Command {
     Ping,
     Set(String, Value),
     Get(String),
     Del(String),
+}
+
+#[derive(Debug, PartialEq)]
+pub enum CommandError {
+    NotAnArray,
+    EmptyCommand,
+    InvalidCommandName,
+    UnknownCommand(String),
+    WrongArity,
 }
 
 pub enum Response {
@@ -18,4 +30,82 @@ pub enum Response {
     Get(Option<Value>),
     Del(bool),
     Error(String),
+}
+
+impl Command {
+    pub fn from_frame(frame: RespFrame) -> Result<Command, CommandError> {
+        let frames = match frame {
+            RespFrame::Arrays(v) => v,
+            _ => return Err(CommandError::NotAnArray),
+        };
+
+        if frames.is_empty() {
+            return Err(CommandError::EmptyCommand);
+        }
+
+        let name = match &frames[0] {
+            RespFrame::BulkStrings(Some(s)) => s.to_ascii_uppercase(),
+            _ => return Err(CommandError::InvalidCommandName),
+        };
+
+        match name.as_str() {
+            "PING" => Ok(Command::Ping),
+            _ => Err(CommandError::UnknownCommand(name)),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ping_from_frame() {
+        let frame = RespFrame::Arrays(vec![
+            RespFrame::BulkStrings(Some("PING".into())),
+        ]);
+        assert_eq!(Command::from_frame(frame), Ok(Command::Ping));
+    }
+
+    #[test]
+    fn ping_case_insensitive() {
+        let frame = RespFrame::Arrays(vec![
+            RespFrame::BulkStrings(Some("ping".into())),
+        ]);
+        assert_eq!(Command::from_frame(frame), Ok(Command::Ping));
+    }
+
+    #[test]
+    fn not_an_array() {
+        let frame = RespFrame::SimpleString("PING".into());
+        assert_eq!(Command::from_frame(frame), Err(CommandError::NotAnArray));
+    }
+
+    #[test]
+    fn empty_array() {
+        let frame = RespFrame::Arrays(vec![]);
+        assert_eq!(Command::from_frame(frame), Err(CommandError::EmptyCommand));
+    }
+
+    #[test]
+    fn unknown_command() {
+        let frame = RespFrame::Arrays(vec![
+            RespFrame::BulkStrings(Some("FOOBAR".into())),
+        ]);
+        assert_eq!(
+            Command::from_frame(frame),
+            Err(CommandError::UnknownCommand("FOOBAR".into()))
+        );
+    }
+
+    #[test]
+    fn invalid_command_name() {
+        let frame = RespFrame::Arrays(vec![
+            RespFrame::Integer(42),
+        ]);
+        assert_eq!(
+            Command::from_frame(frame),
+            Err(CommandError::InvalidCommandName)
+        );
+    }
 }
